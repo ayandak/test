4a9637fb6b0a01e88bc75d900abf0b9fafde65d6
Ayanda Khaka <ayanda.khaka@kineticskunk.com>
2016-02-10 14:47:08 +0200

    second commit

--- src/HelloChurch.java
+++ src/HelloChurch.java
@@ -3,7 +3,7 @@ public class HelloChurch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hellow Church how are you doing");
+		System.out.println("Hello Ayanda");
 	}
 
 }
