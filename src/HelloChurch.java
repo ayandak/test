a05e9e407633c4f62ee3532d8b1bea5221c6a540
ayandak <ayanda.khaka@kineticskunk.com>
2016-02-10 15:53:08 +0200

    Update HelloChurch.java
    done with pull

--- src/HelloChurch.java
+++ src/HelloChurch.java
@@ -3,7 +3,7 @@ public class HelloChurch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Ayanda khaka has done the pull well");
+		System.out.println("now I understand git");
 	}
 
 }
