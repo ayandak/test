1658a015d521b805dbafe1add8e57fb9712a2649
Ayanda Khaka <ayanda.khaka@kineticskunk.com>
2016-02-10 15:45:12 +0200

    edited

--- src/HelloChurch.java
+++ src/HelloChurch.java
@@ -3,7 +3,7 @@ public class HelloChurch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Ayanda Khaka How are you TODAY");
+		System.out.println("Hello Ayanda khaka has done the pull well");
 	}
 
 }
