5367287b07aa4fa302880f621cdd287ec768192c
Ayanda Khaka <ayanda.khaka@kineticskunk.com>
2016-02-11 10:09:36 +0200

    made some changes

--- src/HelloChurch.java
+++ src/HelloChurch.java
@@ -4,6 +4,10 @@ public class HelloChurch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("now I understand git");
+		int a = 5;
+		int b = 5;
+		int sum = a+b;
+		System.out.println("sum is " + sum);
 	}
 
 }
