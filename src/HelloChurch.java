0b0f4a37bf96fc9bb4536985ea3219f09bad429e
silo <ayandakhakha@gmail.com>
2016-02-11 14:44:18 +0200

    added a commit

--- src/HelloChurch.java
+++ src/HelloChurch.java
@@ -4,7 +4,7 @@ public class HelloChurch {
 		int a = 5;
 		int b = 5;
 		int sum = a+b;
-		System.out.println("sum is " + sum);
+		System.out.println("sum is equal " + sum);
 		if(sum==10){
 			System.out.println("sum is equal 10");
 		}
