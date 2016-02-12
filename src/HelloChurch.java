7b82a04ccfa6a6e766c05fa72e48cd80a2541e1a
ayandak <ayanda.khaka@kineticskunk.com>
2016-02-12 15:05:30 +0200

    Update HelloChurch.java

--- src/HelloChurch.java
+++ src/HelloChurch.java
@@ -4,7 +4,7 @@ public class HelloChurch {
 		int a = 8;
 		int b = 5;
 		int sum = a+b;
-		System.out.println("This is where i did the changes and i made anothe changes");
+		System.out.println("This is where i did the changes");
 	}
 
 	public static void main(String[] args) {
